de9debdbc56689567cc1a2316b49203e84ee9131
Kate Koch <kate@katekoch.com>
2019-04-02 18:25:25 -0500

    working front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'react-materialize';
+import Nav from "../src/components/Nav/index";
+import HomePage from '../src/components/HomePage/index';
+import Footer from '../src/components/Footer/index';
 import './App.css';
+import './index.css'
+
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Nav/>
+     
+         <HomePage/>
+          <Footer/> 
         </header>
       </div>
     );
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,81 @@
+import React from "react"
+// import bg3 from "../../background3.jpg"
+
+
+function HomePage(){
+    return(
+    <div className="homepage-container">
+        <div className="section no-pad-bot" id="index-banner">
+          <div className="container">     
+            <h1 className="header center orange-text">Ahhhh jusss book it!</h1>
+          </div>
+        </div>
+    
+    
+      <div className="container">
+        <div className="section">
+    
+
+          
+          <div className="row">
+            <div className="col s12 m4">
+              <div className="icon-block">
+              
+                <h2 className="center brown-text"><img src={'http://pixel.nymag.com/imgs/daily/intelligencer/2015/10/29/magazine/30-willie-nelson.w1200.h630.jpg'}  height="200px"/></h2>
+                <h5 className="center">Willie Nelson</h5>
+    
+                 </div>
+            </div>
+            <div className="col s12 m4">
+              <div className="icon-block">
+              <h2 className="center brown-text"><img src={'http://www.culturesnob.net/assets/images/entries/2008/04/spoon.jpg'}  height="200px"/></h2>
+                 <h5 className="center">Spoon</h5>
+                   </div>
+            </div>
+              <div className="col s12 m4">
+              <div className="icon-block">
+              <h2 className="center brown-text"><img src={'https://i1.wp.com/www.7thlevelmusic.com/wp-content/uploads/2018/04/RD.jpg?resize=768%2C681&ssl=1'}  height="200px"/></h2>
+              <h5 className="center">Ringo Deathstarr</h5>
+    
+               </div>
+            </div>
+            <div className="col s12 m4">
+              <div className="icon-block">
+              <h2 className="center brown-text"><img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Explosions_in_the_Sky.jpg/440px-Explosions_in_the_Sky.jpg'}  height="200px"/></h2>
+                 <h5 className="center">Explosions in the Sky</h5>
+    
+                 </div>
+            </div>
+
+            <div className="col s12 m4">
+              <div className="icon-block">
+            <h2 className="center brown-text"><img src={'http://bluenotejazzfestival.com/wp-content/uploads/2012/04/BNJF-Bonerama.jpg'}  height="200px"/></h2>
+               <h5 className="center">Mingo Fishtrap</h5>
+   
+    
+                 </div>
+            </div>
+            <div className="col s12 m4">
+              <div className="icon-block">
+              <h2 className="center brown-text"><img src={'http://www.wfuv.org/sites/default/files/34278974971_8bd64a29b0_k.jpg'}  height="200px"/></h2>
+               <h5 className="center">The Black Angels</h5>
+    
+                 </div>
+            </div>
+          </div>
+    
+        </div>
+      </div>
+  
+      <div className="container">
+        <div className="section">
+    
+        </div>
+      </div>
+    
+    
+      
+      </div>
+    )
+}
+export default HomePage
